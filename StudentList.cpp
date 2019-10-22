6dec37a19f274a628eeb9e178a97e9c73dec4916
U-BSD\463293 <463293@SHS-10DEC7FA8.beaverton.k12.or.us>
2019-10-22 09:11:04 -0700

    StudentList.cpp

--- StudentList.cpp
+++ StudentList.cpp
@@ -6,11 +6,23 @@ void quitProgram();
 void addStudent();
 void printStudent();
 void deleteStudent();
+double charArrToNum(char*);
 
+struct Student{
+  char* fname;
+  char* lname;
+  int id;
+  double gpa;
+};
+  
 int main() {
-  char keyword[10];
-  bool stillPlaying == true;
-  while(true){
+  char* fname = new char[20];
+  char* lname = new char[20];
+  char* id = new char[7];
+  char* gpa = new char[10];
+  char* keyword = new char[10];
+  bool stillPlaying = true;
+  while(stillPlaying == true){
   cout << "Type in a keyword (\"ADD\", \"PRINT\",\"DELETE\", or \"QUIT\")"<<endl;
   cin.get(keyword, 10); //put into array of 10. Extra char will be ignored
   cin.clear(); //clear, ignore fixes null bug
@@ -34,37 +46,60 @@ int main() {
   else{
     cout << "Make sure the keyword is capitalized." << endl;
   }
-}
+  }
   return 0;
 }
 
 void addStudent(){
-  char fname[20];
-  char lname[20];
-  char id[7];
-  char gpa[10];
+  Student* student = new Student();
+  char* fname = new char[20];
+  char* lname = new char[20];
+  char* id = new char[7];
+  char* gpa = new char[10];
+  double numId = 0;
   cout << "Enter the first name:" << endl;
   cin.get(fname, 20);
   cin.clear();
   cin.ignore(9999, '\n');
+  student -> fname = fname;
   cout << "Enter the last name:" << endl;
   cin.get(lname, 20);
   cin.clear();
   cin.ignore(9999, '\n');
+  student -> lname = lname;
   cout << "Enter the Student Id:" << endl;
   cin.get(id, 7);
   cin.clear();
   cin.ignore(9999, '\n');
+  charArrToNum(id);
+  while (numId > 999999 || numId < 1000000){
+    cout << "Enter a 6 digit number." << endl;
+  }
+  student -> id = numId;
   cout << "Enter the GPA:" << endl;
   cin.get(gpa, 10);
   cin.clear();
   cin.ignore(9999, '\n');
-  if(gpa[4] < 5){
-    //char to double, add 0.005
+  charArrToNum(gpa);
+  int truncate = gpa[4];
+  if(truncate > 5){
+    gpa[3] = gpa[3] + 1; //char to double, add 0.005
+  }
+  student -> gpa = gpa;
 }
 
-void printStudent(){
-  
+ double charArrToNum(char* arr){
+   double numId = 0;
+   for (int i = 0; i != 0; i++){
+     numId = (arr[i] - '0') + (numId * 10);
+   }
+   return numId;
+ }
+ 
+ void printStudent(char* fname, char* lname, int id, double gpa){
+   Student student;
+   cout << student -> fname << "ID: " << student -> id << "GPA: ";
+   cout << student -> gpa << endl;
 }
 
 void deleteStudent(){
@@ -72,7 +107,7 @@ void deleteStudent(){
 }
 
 void quitProgram(){
+  bool stillPlaying;
   cout << "Have a nice day!" << endl;
-  stillPlaying = false;
-  exit(0); //close program     
+  stillPlaying = false; //close program     
 }
