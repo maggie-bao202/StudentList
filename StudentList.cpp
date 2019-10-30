64856391ce8ca05dfac7706cd405cbb22bc4c269
U-BSD\463293 <463293@SHS-10DEC7FA8.beaverton.k12.or.us>
2019-10-30 09:12:55 -0700

    finsih delete

--- StudentList.cpp
+++ StudentList.cpp
@@ -2,17 +2,18 @@
 #include <cstring>
 #include <vector>
 #include<iterator>
+#include<iomanip>
 using namespace std;
 
 struct Student{
-  char* fname;
-  char* lname;
+  char fname[40];
+  char lname[40];
   int id;
   float gpa;
 };
 
-void addStudent(vector<Student*>);
-void printStudent(vector<Student*>);
+void addStudent(vector<Student*>*);
+void printStudent(vector<Student*>*);
 void deleteStudent();
 //double charArrToNum(char*);
   
@@ -26,85 +27,86 @@ int main() {
   
   bool stillPlaying = true;
   while(stillPlaying == true){
-  cout << "Type in a keyword (\"ADD\", \"PRINT\",\"DELETE\", or \"QUIT\")"<<endl;
-  cin.get(keyword, 10); //put into array of 10. Extra char will be ignored
-  cin.clear(); //clear, ignore fixes null bug
-  cin.ignore(9999, '\n');
-  if (strcmp(keyword, "ADD") == 0){
-    //insert addStudent method
-    addStudent(studentList);
-  }
-  else if (strcmp(keyword, "PRINT") == 0){
-    //insert printStudent method
-    printStudent(studentList);
-  }
-  else if (strcmp(keyword, "DELETE") == 0){
-    //insert deleteStudent method
-    deleteStudent();
-  }
-  else if(strcmp(keyword, "QUIT") == 0){
-     cout << "Have a nice day!" << endl;
-     stillPlaying = false; //close program
+    cout << "Type in a keyword (\"ADD\", \"PRINT\",\"DELETE\", or \"QUIT\")"<<endl;
+    cin.get(keyword, 10); //put into array of 10. Extra char will be ignored
+    cin.clear(); //clear, ignore fixes null bug
+    cin.ignore(9999, '\n');
+    if (strcmp(keyword, "ADD") == 0){
+      //insert addStudent method
+      addStudent(&studentList);
+    }
+    else if (strcmp(keyword, "PRINT") == 0){
+      //insert printStudent method
+      printStudent(&studentList);
+    }
+    else if (strcmp(keyword, "DELETE") == 0){
+      //insert deleteStudent method
+      deleteStudent();
+    }
+    else if(strcmp(keyword, "QUIT") == 0){
+      cout << "Have a nice day!" << endl;
+      stillPlaying = false; //close program
 
-  }
-  else{
-    cout << "Make sure the keyword is capitalized." << endl;
-  }
+    }
+    else{
+      cout << "Make sure the keyword is capitalized." << endl;
+    }
   }
   return 0;
 }
 
-void addStudent(vector<Student*> studentList){
-  char first[20] = "Maggie";
-  char last[20] = "Bao";
-  int id = 463293;
-  float gpa = 2.22;
+void addStudent(vector<Student*>* studentList){
   Student* student = new Student();
-  cout << "Enter the first name:" << endl;
-  //cin.get(first, 20);
-  //cin.clear();
-  //cin.ignore(9999, '\n');
-  student -> fname = first;
-  cout << "Enter the last name:" << endl;
-  //cin.get(last, 20);
-  //cin.clear();
-  //cin.ignore(9999, '\n');
-  student -> lname = last;
-  cout << "Enter the Student Id:" << endl;
-  //cin << id;
-  //while (id > 1000000 || id < 99999){
-  //cout << "Enter a 6 digit number." << endl;
-  //cin << id;
-  //}
-  student -> id = id;
-  //cout << "Enter the GPA:" << endl;
-  //cin << gpa;
-  //numId = charArrToNum(* gpa);
-  //gpa = floor(gpa*100+0.5)/100;
-  student -> gpa = gpa;
-  cout << student -> fname << endl;
-  studentList.push_back(student);
-  cout << studentList.size() << endl;
-  
+  int iid = 0;
+  float igpa = 0.0;
+  cout << "Enter the first name: ";
+  cin >> student->fname;
+  cout << "Enter the last name: ";
+  cin >> student->lname;
+  cout << "Enter the Student Id: ";
+  cin >> iid;
+  while (iid > 999999 || iid < 100000){
+    cout << "Enter a 6 digit number." << endl;
+    cout << "Enter the Student Id: ";
+    cin >> iid;
+  }
+  student->id = iid;
+  cout << "Enter the GPA: ";
+  cin >> igpa;
+  while (igpa > 5.0 || igpa < 0.0){
+    cout << "GPA must be less than 5.00 and greater than 0.00.";
+    cout << "Enter the GPA: ";
+    cin >> igpa;
+  }
+  student->gpa = igpa;
+  cin.clear();
+  cin.ignore(999, '\n');
+  studentList->push_back(student);
 }
-
-/*float charArrToNum(char* arr){
-   float numId = 0;
-   for (int i = 0; i < strlen(arr); i++){
-     numId += pow(10, 6-i)*(arr[i] - '0');
-   }
-   return numId;
-   }*/
  
-void printStudent(vector<Student*> studentList) {
+void printStudent(vector<Student*>* studentList) {
    vector<Student*>::iterator it;
-   cout << studentList.size() << endl;
-   for(it = studentList.begin(); it != studentList.end(); it++)    {
-     cout << (*it)->fname << " " << (*it)->lname << "," << (*it) ->id << "," << (*it)->gpa << endl;
+   for(it = studentList->begin(); it != studentList->end(); it++){
+     cout << (*it)->fname << " ";
+     cout << (*it)->lname << ", ";
+     cout << fixed<<setprecision(2);
+     cout << (*it) ->id << ", ";
+     cout << (*it)->gpa << endl;
 
    }
 }
 
-void deleteStudent(){
+void deleteStudent(vector<Student*>* studentList){
+  vector<Student*>::iterator it;
+  char ifname[40];
+  cout << "Enter the first name of a student to remove: ";
+  cin.get(ifname,40);
+  cin.get();
+  for(it = studentList->begin(); it != studentList->end(); it++){
+    if (strcmp((*it)->fname, ifname) == 0){
+      delete(*it);
+      studentList -> erase(it);
+    }
+  }
   
 }
